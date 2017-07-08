504f2554ab9c0b86a987c1bededac0b33e8dc889
fain182 <fain182@gmail.com>
2017-07-08 16:19:45 +0200

    Initial import

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Trello extra is working 3!!");
+
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = ".list-header-num-cards.hide { display: inline-block; }";
+css.innerHTML += ".card-short-id.hide { display: inline-block; }";
+css.innerHTML += ".card-short-id { font-size: 9pt; margin-right: 5px; color: #fff; background: #ccc; width: 51px; text-align: center; border-radius: 3px; float: right; }";
+document.body.appendChild(css);
