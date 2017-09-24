5dd5fd1e865184cfdba1efd8dae711615ffa140b
fain182 <fain182@gmail.com>
2017-09-24 14:02:51 +0200

    fix #2: add card id in detail page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
+var bodyClassObserver = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (isCardDetailPage()) {
+      setCardIdInDetail();
+    }
+  });
+});
 
+var config = {
+  attributes: true,
+  attributeFilter: ['class']
+};
+bodyClassObserver.observe(getBody(), config);
+
+function isCardDetailPage() {
+  var body = getBody();
+  return body.className.includes("body-board-view") && body.className.includes("window-up");
+}
+
+function setCardIdInDetail() {
+  var idRegex = /\/c\/.*\/(\d+)-.*/;
+  var results = idRegex.exec(document.location.pathname);
+  var id = results[1];
+  var cardTitle = document.getElementsByClassName("window-header")[0];
+  cardTitle.setAttribute("data-card-id", "#" + id);
+}
+
+function getBody() {
+  return document.getElementsByTagName('body')[0];
+}
